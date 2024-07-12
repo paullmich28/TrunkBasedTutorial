253a961d4794b73cb9853fa7316e11cd56e2564c
Paulus Michael Leang <99465357+paullmich28@users.noreply.github.com>
2024-07-12 09:44:46 +0700

    Initial Commit

---
+++ TrunkBasedTutorial/TrunkBasedTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrunkBasedTutorialApp.swift
+//  TrunkBasedTutorial
+//
+//  Created by Paulus Michael on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TrunkBasedTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
